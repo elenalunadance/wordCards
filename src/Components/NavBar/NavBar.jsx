125f07ba7513bbf8891ca6864a0065bd5af94779
elenalunadance <elenaluna.dance2023@gmail.com>
2025-09-28 22:17:48 +0300

    Refactor NavBar component to use imported logo and improve code formatting

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,29 @@
-import { Link } from "react-router";
+import { Link } from 'react-router';
 import styles from './navBar.module.css';
+import logo from '../../assets/images/logo.png';
 
-function NavBar( { title = "English words"} ) {
-
-
-    return (
-        <nav className={styles.navBar}>
-            <ul className={styles.list}>
-                <li>
-                    <Link to="/" className={styles.link}><img src="src/assets/images/logo.png" alt="лого" /></Link>
-                </li>
-                <li className={styles.link}>
-                    <Link to="/" className={styles.link}>Home</Link>
-                </li>
-                <li>
-                    <Link to="/game" className={styles.link}>Game</Link>
-                </li>
-            </ul>
-        </nav>
-    );
+function NavBar({ title = 'English words' }) {
+  return (
+    <nav className={styles.navBar}>
+      <ul className={styles.list}>
+        <li>
+          <Link to='/' className={styles.link}>
+            <img src={logo} alt='лого' />
+          </Link>
+        </li>
+        <li className={styles.link}>
+          <Link to='/' className={styles.link}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='/game' className={styles.link}>
+            Game
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
