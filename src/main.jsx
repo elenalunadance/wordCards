d0eb81ef3457a3caa00316ac7257157327639fc6
elenalunadance <elenaluna.dance2023@gmail.com>
2025-09-28 23:11:25 +0300

    Refactor CSS styles for Word component and add global styles; improve code formatting

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App/App';
 import { Provider } from 'mobx-react';
 import { wordsStore } from './store/WordsStore';
-
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider wordsStore={wordsStore}>
       <App />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
